685a57c209f6a13bb08ab903775eec793394e25a
professorbee <batquartet@gmail.com>
2022-02-25 10:17:32 -0500

    Fix for new website
    Calls the new api endpoint, and only makes the request once for the full image list. much more efficient

--- randomplushmiku/myfunctions.py
+++ randomplushmiku/myfunctions.py
@@ -1,9 +1,9 @@
 import requests
+import random
 
 class Miku():
     def __init__(self, url = "https://plushmiku.xyz/"):
         self.url = url
+        self.imageList = requests.get(self.url + "api/images.json").json()["images"]
     def getRandomMiku(self):
-        response = requests.get(self.url + "api/random")
-        data = response.json()
-        return data['username']
+        return self.url + random.choice(self.imageList)[1:]
